fb10a7a27dd4506e2816249c8bec78ef22e1e29a
Gabrielle Miller-Messner <gabriellemiller-messner@Gabrielles-MacBook-Pro.local>
2016-03-03 10:08:09 -0800

    allow video trimming

--- GifMaker_ObjC/GifMaker_ObjC/RecordVideoViewController.m
+++ GifMaker_ObjC/GifMaker_ObjC/RecordVideoViewController.m
@@ -12,6 +12,7 @@
 #import <MobileCoreServices/MobileCoreServices.h>
 #import "DisplayGifViewController.h"
 #import "GifMaker_ObjC-Swift.h"
+@import AVFoundation;
 
 @interface RecordVideoViewController()
 
@@ -67,24 +68,24 @@ static const int kLoopCount = 0; // 0 means loop forever
 
 # pragma mark - UIImagePickerController Delegate methods
 
--(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
-    
-    CFStringRef mediaType = (__bridge CFStringRef)([info objectForKey:UIImagePickerControllerMediaType]);
-    [self dismissViewControllerAnimated:TRUE completion:nil];
-    
-    // Handle a movie capture
-    if (mediaType == kUTTypeMovie) {
-        
-        NSURL *url = [info objectForKey:UIImagePickerControllerMediaURL];
-        self.videoURL = url;
-        NSString *path = url.path;
-        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path)) {
-            UISaveVideoAtPathToSavedPhotosAlbum(path, self, @selector(video: didFinishSavingWithError: contextInfo:), nil);
-        }
-     
-        [self convertVideoToGif];
-    }
-}
+//-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+//    
+//    CFStringRef mediaType = (__bridge CFStringRef)([info objectForKey:UIImagePickerControllerMediaType]);
+//    [self dismissViewControllerAnimated:TRUE completion:nil];
+//    
+//    // Handle a movie capture
+//    if (mediaType == kUTTypeMovie) {
+//        
+//        NSURL *url = [info objectForKey:UIImagePickerControllerMediaURL];
+//        self.videoURL = url;
+//        NSString *path = url.path;
+//        if (UIVideoAtPathIsCompatibleWithSavedPhotosAlbum(path)) {
+//            UISaveVideoAtPathToSavedPhotosAlbum(path, self, @selector(video: didFinishSavingWithError: contextInfo:), nil);
+//        }
+//     
+//        [self convertVideoToGif];
+//    }
+//}
 
 -(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
     [self dismissViewControllerAnimated:TRUE completion:nil];
@@ -104,6 +105,84 @@ static const int kLoopCount = 0; // 0 means loop forever
     displayGifVC.gifURL = self.gifURL;
     [self presentViewController:displayGifVC animated:true completion:nil];
 
+}
+
+// Allows Editing
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    
+    CFStringRef mediaType = (__bridge CFStringRef)([info objectForKey:UIImagePickerControllerMediaType]);
+    [self dismissViewControllerAnimated:TRUE completion:nil];
+    
+    // Handle a movie capture
+    if (mediaType == kUTTypeMovie) {
+        
+        NSURL *rawVideoURL = [info objectForKey:UIImagePickerControllerMediaURL];
+        
+        // Get start and end points from trimmed video
+        NSNumber *start = [info objectForKey:@"_UIImagePickerControllerVideoEditingStart"];
+        NSNumber *end = [info objectForKey:@"_UIImagePickerControllerVideoEditingEnd"];
+
+        // If start and end are nil then clipping was not used.
+        if (start != nil) {
+            int startMilliseconds = ([start doubleValue] * 1000);
+            int endMilliseconds = ([end doubleValue] * 1000);
+        
+
+            AVURLAsset *videoAsset = [AVURLAsset URLAssetWithURL:rawVideoURL options:nil];
+            
+            AVAssetExportSession *exportSession = [[AVAssetExportSession alloc] initWithAsset:videoAsset presetName:AVAssetExportPresetHighestQuality];
+        
+            NSString *outputURL = [self createPath];
+            exportSession.outputURL = [NSURL fileURLWithPath:outputURL];
+            
+            exportSession.outputFileType = AVFileTypeQuickTimeMovie;
+            CMTimeRange timeRange = CMTimeRangeMake(CMTimeMake(startMilliseconds, 1000), CMTimeMake(endMilliseconds - startMilliseconds, 1000));
+            exportSession.timeRange = timeRange;
+            //self.videoURL = exportSession.outputURL;
+        
+            [exportSession exportAsynchronouslyWithCompletionHandler:^{
+                switch (exportSession.status) {
+                    case AVAssetExportSessionStatusCompleted:
+                        // Custom method to import the Exported Video
+                        self.videoURL = exportSession.outputURL;
+                        [self convertVideoToGif];
+                        break;
+                    case AVAssetExportSessionStatusFailed:
+                        //
+                        NSLog(@"Failed:%@",exportSession.error);
+                        break;
+                    case AVAssetExportSessionStatusCancelled:
+                        //
+                        NSLog(@"Canceled:%@",exportSession.error);
+                        break;
+                    default:
+                        break;
+                }
+            }];
+
+            // If video was not trimmer, use the entire video.
+        } else {
+            self.videoURL = rawVideoURL;
+            [self convertVideoToGif];
+        }
+    }
+}
+
+-(NSString*)createPath {
+
+    NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentsDirectory = [paths objectAtIndex:0];
+
+    NSFileManager *manager = [NSFileManager defaultManager];
+
+    NSString *outputURL = [documentsDirectory stringByAppendingPathComponent:@"output"] ;
+    [manager createDirectoryAtPath:outputURL withIntermediateDirectories:YES attributes:nil error:nil];
+
+    outputURL = [outputURL stringByAppendingPathComponent:@"output.mov"];
+    // Remove Existing File
+    [manager removeItemAtPath:outputURL error:nil];
+
+    return outputURL;
 }
 
 @end
